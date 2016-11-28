3b18f37808d96e1af44d11e81741f760f8bcefc0
thomasbatt <thomasbatt@gmail.com>
2016-11-28 14:30:56 +0100

    cleaning

--- index.php
+++ index.php
@@ -5,9 +5,5 @@ if (file_exists("./wordpress/index.php")){
 	exit;
 }
 else
-<<<<<<< HEAD
 	echo "Veuillez suivre les instructions du README.md";
-=======
-	echo "Veuillez telecharger Wordpress et l'installer dans la racine du depot";
->>>>>>> 1123a0cef45d45fb6007544d93df4d57e7736c36
 ?>
